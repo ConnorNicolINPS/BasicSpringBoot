9061ffc18aef3e8fb2e40cfac8723a34606ecc92
ConnorNicolINPS <connor.nicol@inps.co.uk>
2017-08-17 13:12:29 +0100

    Added constant values to the project

--- BasicSpringBootProject/trunk/src/main/java/uk/co/inps/Controllers/HelloWorldController.java
+++ BasicSpringBootProject/trunk/src/main/java/uk/co/inps/Controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package uk.co.inps.Controllers;
 
 import org.springframework.web.bind.annotation.*;
+import uk.co.inps.Utils.Constants;
 
 /**
  * Created by connor.nicol on 17/08/2017.
@@ -8,19 +9,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HelloWorldController
 {
-    @RequestMapping("/hello/{name}")
+    @RequestMapping(value = Constants.HELLO_NAME_INLINE)
     @ResponseBody
     private String helloPathVariable(@PathVariable final String name) {
         return "Hello " + name + "!";
     }
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = Constants.HELLO_NAME_PARAM)
     @ResponseBody
     private String helloRequestParameter(@RequestParam final String name) {
         return "Hello " + name + "!";
     }
 
-    @RequestMapping("/")
+    @RequestMapping(value = Constants.HELLO_WORLD_URL)
     @ResponseBody
     private String helloWorld() {
         return "Hello world!";
---
+++ BasicSpringBootProject/trunk/src/main/java/uk/co/inps/Utils/Constants.java
@@ -0,0 +1,12 @@
+package uk.co.inps.Utils;
+
+public class Constants {
+
+    public Constants() {
+    }
+
+    public static final String HELLO_WORLD_URL = "/";
+    public static final String HELLO_NAME_PARAM = "/hello";
+    public static final String HELLO_NAME_INLINE= "/hello/{name}";
+
+}
